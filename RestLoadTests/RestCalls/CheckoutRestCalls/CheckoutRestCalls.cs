81c659b48684f7e2ebfb54c58d378c598b958f42
chrisessley <chris.essley@mindbodyonline.com>
2013-07-19 12:04:13 -0700

    add checkout shopping cart calls and tests

--- RestLoadTests/RestCalls/CheckoutRestCalls/CheckoutRestCalls.cs
+++ RestLoadTests/RestCalls/CheckoutRestCalls/CheckoutRestCalls.cs
@@ -1,10 +1,30 @@
-﻿namespace RestCalls.CheckoutRestCalls
+﻿using System.Globalization;
+using RestCalls.RestObjects;
+using RestSharp;
+
+namespace RestCalls.CheckoutRestCalls
 {
     public class CheckoutRestCalls : AbstractBaseRestSetup
     {
-        public string CheckoutShoppingCart()
+        public IRestResponse CheckoutShoppingCart(int siteId, RestShoppingCart cart)
         {
-            return "";
+            var client = new RestClient("http://dev2-connect.mbodev.me");
+
+            var request = new RestRequest("/rest/sale/Checkout", Method.POST) { RequestFormat = DataFormat.Json };
+
+            request.AddHeader("Content-type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + UserAccessToken);
+            request.AddHeader("SiteId", siteId.ToString(CultureInfo.InvariantCulture));
+
+            request.AddBody(new
+                {
+                    seriesID = cart.SeriesId,
+                    amount = cart.Amount,
+                    userID = cart.UserId,
+                    userBillingInfoID = cart.UserBillingInfoId
+                });
+
+            return client.Execute(request);
         }
     }
 }
---
+++ RestLoadTests/RestCallsTests/CheckoutRestCallsTests/CheckoutRestCallsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MbUnit.Framework;
+using RestCalls.CheckoutRestCalls;
+using RestCalls.RestObjects;
+using RestSharp;
+
+namespace RestCallsTests.CheckoutRestCallsTests
+{
+    public class CheckoutRestCallsTests : AbstractRestCallTestSuite
+    {
+        [Test]
+        public void CheckoutShoppingCartTest()
+        {
+            int siteId = -40000;
+
+            //needs real mock data as do all tests 7-19-13
+            RestShoppingCart cart = new RestShoppingCart { SeriesId = 234, Amount = 3.50, UserId = UserId, UserBillingInfoId = 4 };
+
+            CheckoutRestCalls checkoutRestCalls = new CheckoutRestCalls();
+
+            IRestResponse response = checkoutRestCalls.CheckoutShoppingCart(siteId, cart);
+
+            Console.WriteLine(response.Content);
+
+            Assert.AreNotEqual(0, response.ContentLength);
+        }
+    }
+}
